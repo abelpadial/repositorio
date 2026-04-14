dd72f983e417301c3450b079e380fc9d1bd06398
Abel <abelpadial1@gmail.com>
2026-04-14 09:49:38 +0200

    Añadido imprimirarray y nuevometodo1

--- array.java
+++ array.java
@@ -9,4 +9,12 @@ public class Array {
 			array[i] = rand.nextInt(20) + 1;
 		}
 	}
+public void imprimirarray(){
+for (int i = 0; i < array.length; i++){
+System.out.println(array[i]);
+    }
+  }
+public void nuevometodo1(){
+ 	System.out.println("Metodo nuevo 1 ¡¡¡¡¡¡¡¡¡modificado!!!!!!!!!!");
+ }
 }
